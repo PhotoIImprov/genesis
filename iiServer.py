4810ab34e3df0dece9dbcb535306dd96539595ad
Harry Collins <bp100a@hotmail.com>
2017-02-03 12:08:46 -0500

    initial commit

---
+++ iiServer.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify
+from flask_jwt import JWT, jwt_required, current_identity
+from flask     import request, abort
+
+import usermgr
+
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'super-secret'
+
+# specify the JWT package's call backs for authentication of username/password
+# and subsequent identity from the payload in the token
+jwt = JWT(app, usermgr.authenticate, usermgr.identity)
+
+@app.route("/protected")
+@jwt_required()
+def protected():
+    return '%s' % current_identity
+
+@app.route("/register", methods=['POST'])
+def register():
+    # an email address and password has been posted
+    # let's create a user for this
+    if not request.json:
+        abort(400)  # no data passed!
+
+    emailaddress = request.json['username']
+    password     = request.json['password']
+    if emailaddress is None or password is None:
+        abort(400) # missing important data!
+
+    foundUser = usermgr.User().FindUserByEmail(usermgr.Session(), emailaddress)
+    if foundUser is not None:
+        abort(400)  # user exists!
+
+    # okay the request is valid and the user was not found, so we can
+    # create their account
+    newUser = usermgr.User().CreateUser(usermgr.Session(), emailaddress, password)
+    if newUser is None:
+        abort(500)
+
+    # user was properly created
+    return 'account created', 201
+
+if __name__ == '__main__':
+    usermgr.metadata.create_all(bind=usermgr.engine, checkfirst=True)
+
+    new_user = usermgr.User()
+    session = usermgr.Session()
+    new_user.CreateUser(session, 'hcollins', 'pa55w0rd')
+    session.commit()
+
+    # see if we can read it back
+    foundUser = usermgr.User().FindUserByEmail(session, 'hcollins@gmail.com')
+
+    if foundUser != None:
+        foundUser.ChangePassword('pa55w0rd')
+        session.commit()
+
+    app.run(host='0.0.0.0')
+
---
+++ usermgr.py
@@ -0,0 +1,110 @@
+import uuid
+from flask             import Flask
+from flask_jwt         import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
+from passlib.hash      import pbkdf2_sha256
+from sqlalchemy        import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm    import sessionmaker, query
+from sqlalchemy        import Column, Integer, String, DateTime, text
+import datetime
+
+# connection to MySQL instance on 4KOffice (intranet)
+engine  = create_engine('mysql+pymysql://python:python@192.168.1.149:3306/imageimprov', echo=False)
+Session = sessionmaker(bind=engine)
+Base    = declarative_base()
+metadata = Base.metadata
+
+class User(Base):
+
+    __tablename__ = 'userlogin'
+
+    id           = Column(Integer, primary_key = True, autoincrement=True)
+    hashedPWD    = Column(String(200), nullable=False)
+    emailaddress = Column(String(200), nullable=False, unique=True)
+    screenname   = Column(String(100), nullable=True)
+    created_date = Column(DateTime, server_default=text('CURRENT_TIMESTAMP'), nullable=False)
+    last_updated = Column(DateTime, nullable=True, server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP') )
+
+
+#    def __init__(self, id, username, password):
+#        self.id           = id
+#        self.screenname   = username
+#        self.hashedPWD    = pbkdf2_sha256.encrypt(password, rounds=1000, salt_size=16)
+#        self.emailaddress = self.screenname + '@hotmail.com'
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+    def FindUserById(self, session, id):
+        self.id = id
+        q = session.query(User).filter(User.id == self.id)
+        u = q.all()
+
+        if not u:
+            return None
+        else:
+            return u[0]
+
+    def FindUserByEmail(self, session, emailaddress):
+        # Does the user already exist?
+        self.emailaddress = emailaddress
+        q = session.query(User).filter(User.emailaddress == self.emailaddress)
+        u = q.all()
+
+        if not u:
+            return None
+        else:
+            return u[0]
+
+    def ChangePassword(self, password):
+        self.hashedPWD = pbkdf2_sha256.encrypt(password, rounds=1000, salt_size=16)
+
+    def CreateUser(self, session, username, password):
+        # first need to see if user (emailaddress) already exists
+
+        # quick & dirty validation of the email
+        if '@' not in username:
+            return None
+
+        # now hash the password
+        self.hashedPWD    = pbkdf2_sha256.encrypt(password, rounds=1000, salt_size=16)
+        self.screenname   = None
+        self.emailaddress = username
+        self.id           = None
+
+        # first lets see if this user already exists
+        u_exists = self.FindUserByEmail(session, self.emailaddress)
+        if (u_exists is None):
+            # Now write the new users to the database
+            session.add(self)
+            session.commit()
+        else:
+            return None #self.__dict__.update(u_exists.__dict__)
+
+        return self
+
+# This is where all authentication calls come, we need to validate the user
+def authenticate(username, password):
+    # use the username (email) to lookup the passowrd and compare
+    # after we hash the one we were sent
+    foundUser = User().FindUserByEmail(Session(), username)
+
+    if foundUser is not None:
+        # time to check the password
+        if (pbkdf2_sha256.verify(password, foundUser.hashedPWD)):
+            return foundUser
+
+    return None
+
+# subsequent calls with JWT payload call here to confirm identity
+def identity(payload):
+    # called with decrypted payload to establish identity
+    # based on a user id
+    user_id = payload['identity']
+    u = User().FindUserByID(Session(), user_id)
+
+    # if u doesn't exist, that's really bad, it's a corrupted token or something
+    # really strange. We should log this
+    return u
+
