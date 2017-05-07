7419241ac5fb94e760f9eeec3fd928213c17bc0d
hcollins <bp100a@hotmail.com>
2017-05-07 16:48:39 -0400

    increment release to 0.4.1

--- iiServer.py
+++ iiServer.py
@@ -32,7 +32,7 @@ app.config['SECRET_KEY'] = 'imageimprove3077b47'
 
 is_gunicorn = False
 
-__version__ = '0.4.0' #our version string PEP 440
+__version__ = '0.4.1' #our version string PEP 440
 
 
 def fix_jwt_decode_handler(token):
