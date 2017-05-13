c03a7e6037c711bc70331de38ca84d1fa71b8f63
hcollins <bp100a@hotmail.com>
2017-05-13 09:53:30 -0400

    - remove leaderboard creation from iiServer - sync_daemon reads categorys & populates redis server as necessary - more photos! - added retry to initializedata, was getting 502s

--- daemon/sync_leaderboard.py
+++ daemon/sync_leaderboard.py
@@ -1,24 +1,48 @@
 import sys
 import time
 import os
-import daemon
-from models import category, usermgr, photo, voting
-import dbsetup.py
-from datetime import timedelta, datetime, voting
 
-_SCHEDULED_TIME_SECONDS = 60 * 10 # 10 minutes
+lib_path = os.path.abspath(os.path.join('..'))
+sys.path.append(lib_path)
+
+from python_daemon import Daemon
+import redis
+from models import category, usermgr, photo, voting
+import dbsetup
+from datetime import timedelta, datetime
+
+_SCHEDULED_TIME_SECONDS_DEV = 1
+_SCHEDULED_TIME_SECONDS_PROD = 60 * 10 # 10 minutes
 _PAGE_SIZE_PHOTOS = 1000
 _THROTTLE_UPDATES_SECONDS = 0.010 # 10 milliseconds between '_PAGE_SIZE_PHOTOS' record updates
 
-
 class sync_daemon(Daemon):
 
+    _pidf = None
+    _logf = None
+    _redis_host = None
+    _redis_port = None
+    _redis_conn = None
+    _current_lbname = None
+
+    def __init__(self,*args, **kwargs):
+        self._pidf = kwargs.get('pidf')
+        self._logf = kwargs.get('logf')
+
     def run(self):
+        env = dbsetup.determine_environment(None)
+        if env == dbsetup.EnvironmentType.DEV:
+            schedule_time = _SCHEDULED_TIME_SECONDS_DEV
+        else:
+            schedule_time = _SCHEDULED_TIME_SECONDS_PROD
+
+        print ("sleep time %d seconds" % (schedule_time))
+
         while True:
-            sleep(_SCHEDULED_TIME_SECONDS)
+            time.sleep(schedule_time)
             session = dbsetup.Session()
             try:
-                perform_task(session)
+                self.perform_task(session)
                 session.commit()
             except:
                 session.rollback()
@@ -27,8 +51,9 @@ class sync_daemon(Daemon):
 
     def perform_task(self, session):
         cl = self.read_all_categories(session)
+        tm = voting.TallyMan()
         for c in cl:
-            self.scored_photos_by_category(session, c)
+            self.scored_photos_by_category(session, tm, c)
 
     def read_all_categories(self, session):
         earliest_category = datetime.now() + timedelta(days=-7)
@@ -36,31 +61,48 @@ class sync_daemon(Daemon):
             filter(category.Category.state != category.CategoryState.UNKNOWN.value)
         return q.all()
 
-    def leaderboard_exists(self,tm, c):
-        lb_name = tm.leaderboard_name(c)
+    def leaderboard_exists(self,session, tm, c):
+        if self._redis_conn is None:
+            sl = voting.ServerList()
+            d = sl.get_redis_server(session)
+            self._redis_host = d['ip']
+            self._redis_port = d['port']
+            self._redis_conn = redis.Redis(host=self._redis_host, port=self._redis_port)
 
-    def scored_photos_by_category(self, session, c):
+        self._current_lbname = tm.leaderboard_name(c)
+        return self._redis_conn.exists(self._current_lbname)
+
+    def scored_photos_by_category(self, session, tm, c):
         # there could be millions of records, so we need to page
-        more_photos = True
-        tm = voting.TallyMan()
-        if leaderboard_exists(tm, c):
+        print ("category %d" % (c.id))
+        if self.leaderboard_exists(session, tm, c):
             return
+        print("leaderboard does not exist for category %d" % (c.id))
+        tm.get_leaderboard_by_category(session, c)
 
+        more_photos = True
         max_pid = 0
         while more_photos:
+            print("...read 1000 records")
             q = session.query(photo.Photo).filter(photo.Photo.category_id == c.id).\
                 filter(photo.Photo.score > 0).\
                 filter(photo.Photo.id > max_pid).order_by(photo.Photo.id.asc()).limit(_PAGE_SIZE_PHOTOS)
             pl = q.all()
-            more_photos = pl is not None
-            max_pid = pl[0].pid
-            for p in pl:
-                tm.update_leaderboard(session, c, p)
-                max_pid = p.id
-            sleep(_THROTTLE_UPDATES_SECONDS) # brief pause so machine can catch it's breath
+            more_photos = len(pl) > 0
+            if more_photos:
+                max_pid = pl[0].id
+                for p in pl:
+                    tm.update_leaderboard(session, c, p, check_exist=False)
+                    max_pid = p.id
+
+            time.sleep(_THROTTLE_UPDATES_SECONDS) # brief pause so machine can catch it's breath
+
+# ================================================================================================================
 
 if __name__ == "__main__":
     daemon = sync_daemon(pidf='/var/run/synchronize_iiDaemon.pid', logf='/var/log/synchronize_iiDaemon.log')
+
+    daemon.run()
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
             deamon.start()
