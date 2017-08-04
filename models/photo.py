7e3f827471520611bbf15c1699365e692309f9f0
hcollins <bp100a@hotmail.com>
2017-08-04 14:50:15 -0400

    - uncomment make_dummy_exif()

--- models/photo.py
+++ models/photo.py
@@ -457,30 +457,30 @@ class Photo(Base):
     #     status = cv2.imwrite(fn, img)
     #     return status
     #
-    # def make_dummy_exif(self) -> dict:
-    #     zeroth_ifd = {piexif.ImageIFD.Make: u"Unknown",
-    #                   piexif.ImageIFD.Orientation: 1,
-    #                   piexif.ImageIFD.XResolution: (96, 1),
-    #                   piexif.ImageIFD.YResolution: (96, 1),
-    #                   piexif.ImageIFD.Software: u"piexif"
-    #                   }
-    #     exif_ifd = {piexif.ExifIFD.DateTimeOriginal: u"2099:09:29 10:10:10",
-    #                 piexif.ExifIFD.LensMake: u"LensMake",
-    #                 piexif.ExifIFD.Sharpness: 65535,
-    #                 piexif.ExifIFD.LensSpecification: ((1, 1), (1, 1), (1, 1), (1, 1)),
-    #                 }
-    #     gps_ifd = {piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
-    #                piexif.GPSIFD.GPSAltitudeRef: 1,
-    #                piexif.GPSIFD.GPSDateStamp: u"1999:99:99 99:99:99",
-    #                }
-    #     first_ifd = {piexif.ImageIFD.Make: u"Unknown",
-    #                  piexif.ImageIFD.XResolution: (40, 1),
-    #                  piexif.ImageIFD.YResolution: (40, 1),
-    #                  piexif.ImageIFD.Software: u"piexif"
-    #                  }
-    #     exif_dict = {"0th": zeroth_ifd, "Exif": exif_ifd, "GPS": gps_ifd, "1st": first_ifd}
-    #
-    #     return exif_dict
+    def make_dummy_exif(self) -> dict:
+        zeroth_ifd = {piexif.ImageIFD.Make: u"Unknown",
+                      piexif.ImageIFD.Orientation: 1,
+                      piexif.ImageIFD.XResolution: (96, 1),
+                      piexif.ImageIFD.YResolution: (96, 1),
+                      piexif.ImageIFD.Software: u"piexif"
+                      }
+        exif_ifd = {piexif.ExifIFD.DateTimeOriginal: u"2099:09:29 10:10:10",
+                    piexif.ExifIFD.LensMake: u"LensMake",
+                    piexif.ExifIFD.Sharpness: 65535,
+                    piexif.ExifIFD.LensSpecification: ((1, 1), (1, 1), (1, 1), (1, 1)),
+                    }
+        gps_ifd = {piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
+                   piexif.GPSIFD.GPSAltitudeRef: 1,
+                   piexif.GPSIFD.GPSDateStamp: u"1999:99:99 99:99:99",
+                   }
+        first_ifd = {piexif.ImageIFD.Make: u"Unknown",
+                     piexif.ImageIFD.XResolution: (40, 1),
+                     piexif.ImageIFD.YResolution: (40, 1),
+                     piexif.ImageIFD.Software: u"piexif"
+                     }
+        exif_dict = {"0th": zeroth_ifd, "Exif": exif_ifd, "GPS": gps_ifd, "1st": first_ifd}
+
+        return exif_dict
 
     def read_photo_to_b64(self) -> bytes:
         '''
