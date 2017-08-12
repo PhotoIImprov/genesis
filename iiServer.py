bc517f163991ea343863b585266b56444b468a72
hcollins <bp100a@hotmail.com>
2017-08-12 11:11:11 -0400

    - v1.3.8 - updated HTML, re-organized www folder, no more Muse!

--- iiServer.py
+++ iiServer.py
@@ -39,7 +39,7 @@ app.config['SECRET_KEY'] = 'imageimprove3077b47'
 
 is_gunicorn = False
 
-__version__ = '1.3.7' #our version string PEP 440
+__version__ = '1.3.8' #our version string PEP 440
 
 
 def fix_jwt_decode_handler(token):
@@ -245,6 +245,10 @@ def hello():
                 "<li>v1.3.7</li>" \
                 "  <ul>" \
                 "  <li>/submissions API</li>" \
+                "  </ul>" \
+                "<li>v1.3.8</li>" \
+                "  <ul>" \
+                "  <li>custom web pages (no Muse!)</li>" \
                 "  </ul>" \
                 "</ul>"
     htmlbody += "<img src=\"/static/python_small.png\"/>\n"
